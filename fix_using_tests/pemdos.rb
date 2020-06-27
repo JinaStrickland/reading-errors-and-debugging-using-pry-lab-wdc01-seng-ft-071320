572cbd1fe466212185cc0707972a64fb66a280f7
Jina Strickland <jina30@gmail.com>
2020-06-27 15:31:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,17 @@
 # don't forget to add: require 'pry'
 require 'pry'
-
+=begin
 def snake_it_up(string)
   if string[0] == "s"
     "s" * 10 + string
+  else
+    string
+  end
+=end
+
+def snake_it_up(small, large)
+  if string[0] == "s"
+    "s" * 10 + small
   else
     string
   end
